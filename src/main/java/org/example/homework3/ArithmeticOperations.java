44283863799dec74b4fcbc570cf2da5dea9ce480
Andrii Kozachok <149764879+AndrewQAsxope@users.noreply.github.com>
2024-03-25 20:19:42 +0200

    Homework3

---
+++ src/main/java/org/example/homework3/ArithmeticOperations.java
@@ -0,0 +1,20 @@
+package org.example.homework3;
+
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        int number1 = 4;
+        int number2 = 2;
+
+        int sum = number1 + number2;
+        System.out.println("Sum: " + sum);
+
+        int subtraction = number1 - number2;
+        System.out.println("Subtraction: " + subtraction);
+
+        int multiplication = number1 * number2;
+        System.out.println("Multiplication: " + multiplication);
+
+        double division = (double) number1 / number2;
+        System.out.println("Division: " + division);
+    }
+}
---
+++ src/main/java/org/example/homework3/MaxMinNumeric.java
@@ -0,0 +1,12 @@
+package org.example.homework3;
+
+public class MaxMinNumeric {
+    public static void main(String[] args) {
+        int number1 = 10;
+        int number2 = 21;
+
+        System.out.println("Minimum: " + Math.min(number1, number2));
+
+        System.out.println("\nMaximum: "+ Math.max(number1, number2));
+    }
+}
